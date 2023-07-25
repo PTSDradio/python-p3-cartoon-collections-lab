ccdb656d2c731ed30b36af6584335902d735e95b
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-07-25 18:00:49 -0500

    completes lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,26 @@
-def roll_call_dwarves():
+def roll_call_dwarves(list):
+    for name in list:
+        print(f"{list.index(name) + 1}. {name}")
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(list):
+    new_list = []
+    for item in list:
+        new_list.append(f"{item.capitalize()}!")
+    return new_list
     pass
 
-def long_planeteer_calls():
+def long_planeteer_calls(list):
+    value = False
+    for name in list:
+        if len(name) > 4:
+            value= True
+    return value
+    
     pass
 
-def find_the_cheese():
+def find_the_cheese(list):
+    for item in list:
+        if item == "cheddar" or item == "gouda" or item == "cheese" or item == "camembert":
+            return item
     pass
